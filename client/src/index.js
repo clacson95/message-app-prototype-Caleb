3e8d2aa588b9b44ae11dfb5f17fcfb22d6b47a55
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-04 17:08:00 -0500

    small change

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
