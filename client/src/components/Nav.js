2a2a47ec78b852846298a9080af29f87b6910f3e
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-05 20:24:09 -0500

    keeping up to date
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,31 +1,27 @@
 // All conditional rendering for the drawer sidebar in Body
 import React from "react";
-
-// Navigation will be the navbar that appears on each page
+import Conversations from "./Conversations";
+import Contacts from "./Contacts";
 
 function Nav({ currentPage, handlePageChange }) {
-
   return (
     <div className="navbar">
       <div className="links">
         <a
           href="#conversations"
           onClick={() => handlePageChange("Conversations")}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === "Conversations" ? "navbar a" : "navbar"}
         >
-          
+          <Conversations />
         </a>
 
         <a
           href="#contacts"
           onClick={() => handlePageChange("Contacts")}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === "Contacts" ? "navbar a" : "navbar"}
         >
-          
+          <Contacts />
         </a>
-
       </div>
     </div>
   );
