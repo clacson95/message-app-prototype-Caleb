c0ceeb9caea4608d310b59d9a482e8430c4faaec
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-07 16:09:13 -0500

    refactor
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,42 +1,23 @@
+// Import Express
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
+const dotenv = require("dotenv");
+const connectDB = require("./config/connection");
+const userRoutes = require("./routes/userRoutes");
 
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
-
-const PORT = process.env.PORT || 3001;
+// Create an instance of express
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+dotenv.config();
+connectDB();
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.send("API running successfully!");
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
+app.use("/api/user", userRoutes);
 
-  db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
-  });
-};
+const PORT = process.env.PORT;
 
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
+app.listen(console.log(`Server running on port ${PORT}`));
