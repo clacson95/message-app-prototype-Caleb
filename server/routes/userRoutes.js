e92db15a4606e698df369951a65fa603b81234da
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-07 16:38:11 -0500

    starting user auth
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { registerUser } = require("../controllers/userController");
+const { registerUser, authUser } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
-// router.route("/login", authUser)
+router.route("/login", authUser)
 
 module.exports = router;
