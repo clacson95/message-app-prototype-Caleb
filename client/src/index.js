40cb49910e8212fd62d19b9c62bf1d6694a0ee81
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-08 16:27:49 -0500

    chat provider
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,19 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import ChatProvider from "./Context/chatProvider";
+
+// Wrapping the entire app in ChatProvider,
+// ChakraProvider,
+// and BrowserRouter so that these imports are accessible throughout the entire application
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ChakraProvider>
+  <ChatProvider>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </ChatProvider>
 );
