a0986c6556f272e0b612c249e15a18ffecf2775d
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-08 16:26:29 -0500

    added chat routes
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/connection");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 // Create an instance of express
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
