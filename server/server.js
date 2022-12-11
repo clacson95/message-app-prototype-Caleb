ecc6b1b48891829fa1f0000022752dfaef3032d7
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-11 12:16:18 -0500

    message api
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/connection");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 // Create an instance of express
@@ -13,13 +14,13 @@ connectDB();
 
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
   res.send("API running successfully!");
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
