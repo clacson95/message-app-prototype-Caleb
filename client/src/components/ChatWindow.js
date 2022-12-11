a0fb1fa629074756c875426669592cdc2498f55f
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-10 19:42:57 -0500

    chat window ui started
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { ChatState } from "../Context/chatProvider";
+import { Box } from "@chakra-ui/react";
+import SingleChat from "./SingleChat";
 
-const ChatWindow = () => {
+const ChatWindow = ({ fetchAgain, setFetchAgain }) => {
+  const { selectedChat } = ChatState();
+
   return (
-    <div>ChatWindow</div>
-  )
-}
+    <Box
+      d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      Chat Window
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
+  );
+};
 
-export default ChatWindow
+export default ChatWindow;
