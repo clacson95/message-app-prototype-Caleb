882e941cb605c618ed4591662747112a5613c0c5
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-12 15:36:13 -0500

    socket.io implemented
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -14,17 +14,63 @@ connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API running successfully!");
-});
-
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/messages", messageRoutes);
+app.use(userRoutes);
+app.use(chatRoutes);
+app.use(messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(console.log(`Server running on port ${PORT}`));
+// Use app.listen() as an object we can pass through socket.io
+const server = app.listen(PORT, () => {
+  console.log(`API server running on ${PORT}`);
+});
+
+const io = require("socket.io")(server, {
+  // If a user hasn't sent anything in 60 seconds, close the connection to save the bandwidth
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+
+  // Connect user to their personal web socket upon loading the app
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  // Join chat
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined room " + room);
+  });
+
+  // Typing
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  // New message
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+
+    // For debugging
+    if (!chat.users) return console.log("chat.users undefined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+
+    socket.off("setup", () => {
+      console.log("User Disconnected");
+      socket.leave(userData._id);
+    });
+  });
+});
