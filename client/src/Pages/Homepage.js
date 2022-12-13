53c1b446e35627d0eb658d78e7bbcd80cb91ebe6
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-12 21:51:29 -0500

    don't move these
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

---
+++ client/src/Pages/Homepage.js
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import Login from "../components/authentication/Login";
+import Signup from "../components/authentication/Signup";
+import { useHistory } from "react-router-dom";
+
+// ==========================================================
+// ==========================================================
+// Homepage Setup
+// ==========================================================
+// ==========================================================
+const Homepage = () => {
+  // ==========================================================
+  // Local Storage
+  // ==========================================================
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+    // If the user is logged in, push them to the chats page
+    if (user) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          textAlign="center"
+          fontSize="3xl"
+          fontFamily="Work Sans"
+          color="gray.600"
+        >
+          MyloChat ฅ^•ﻌ•^ฅ
+        </Text>
+      </Box>
+
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="green">
+          <TabList mb="1em">
+            <Tab width="70%">Login!</Tab>
+            <Tab width="70%">Sign Up!</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              {" "}
+              <Login />{" "}
+            </TabPanel>
+            <TabPanel>
+              {" "}
+              <Signup />{" "}
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
